017ff1ed55c95b395c1da0c641595bc7895a99a7
UnaAtadura-Eris <78673684+UnaAtadura-Eris@users.noreply.github.com>
2023-05-10 23:02:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('calculateBtn').addEventListener('click', function() {
   // var moment = require('moment');并不需要require或import？
-  var startDate = moment(document.getElementById('startDate').value);
+var startDate = moment(document.getElementById('startDate').value);
   var endDate = moment(document.getElementById('endDate').value);
   var units = document.getElementById('units').value;
   var duration = moment.duration(endDate.diff(startDate));
@@ -8,43 +8,43 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
 
   if (units === 'all' || units === 'year') {
     var years = duration.years();
-    if (years > 0) {
-      result += years + (years === 1 ? ' year ' : ' years ');
+    if (years >= 0) {
+      result += years +  ' 年 ';
     }
   }
 
   if (units === 'all' || units === 'month') {
     var months = duration.months();
-    if (months > 0) {
-      result += months + (months === 1 ? ' month ' : ' months ');
+    if (months >= 0) {
+      result += months +  ' 月 ';
     }
   }
 
   if (units === 'all' || units === 'day') {
     var days = duration.days();
-    if (days > 0) {
-      result += days + (days === 1 ? ' day ' : ' days ');
+    if (days >= 0) {
+      result += days + ' 日 ';
     }
   }
 
   if (units === 'all' || units === 'hour') {
     var hours = duration.hours();
-    if (hours > 0) {
-      result += hours + (hours === 1 ? ' hour ' : ' hours ');
+    if (hours >= 0) {
+      result += hours + ' 时 ';
     }
   }
 
   if (units === 'all' || units === 'minute') {
     var minutes =duration.minutes();
-    if (minutes > 0) {
-    result += minutes + (minutes === 1 ? ' minute ' : ' minutes ');
+    if (minutes >= 0) {
+    result += minutes + ' 分 ';
     }
     }
     
     if (units === 'all' || units === 'second') {
     var seconds = duration.seconds();
-    if (seconds > 0) {
-    result += seconds + (seconds === 1 ? ' second ' : ' seconds ');
+    if (seconds >= 0) {
+    result += seconds + ' 秒 ';
     }
     }
     
